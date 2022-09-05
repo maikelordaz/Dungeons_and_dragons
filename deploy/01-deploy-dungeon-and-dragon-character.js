aee2fa1e68a4ac1c67159b5ad297fcf1d998c9a6
maikelordaz <maikelordaz@gmail.com>
2022-09-04 22:31:31 -0400

    Update 01-deploy-dungeon-and-dragon-character.js

--- deploy/01-deploy-dungeon-and-dragon-character.js
+++ deploy/01-deploy-dungeon-and-dragon-character.js
@@ -1,10 +1,38 @@
-const { verifyMessage } = require("ethers/lib/utils")
-const { network } = require("hardhat")
-const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { network, ethers } = require("hardhat")
+const { verify } = require("../utils/verify")
+const { metadataTemplate } = require("../utils/metadataTemplate")
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
+const {
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    FUND_AMOUNT,
+    networkConfig,
+} = require("../helper-hardhat-config")
+
+const imagesLocation = "./images"
+let characterUris
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        characterUris = await handleCharacterUris()
+    }
+
+    if (chainId == 31337) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const txResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await txResponse.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
 
     log("--------------- Deploying Character Contract... ---------------")
 
@@ -12,7 +40,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         ? 1
         : VERIFICATION_BLOCK_CONFIRMATIONS
 
-    const args = []
+    const args = [
+        vrfCoordinatorV2Address,
+        networkConfig[chainId]["gasLane"],
+        subscriptionId,
+        networkConfig[chainId]["callbackGasLimit"],
+        networkConfig[chainId]["mintFee"],
+        characterUris,
+    ]
 
     const character = await deploy("DungeonAndDragonCharacter", {
         from: deployer,
@@ -24,11 +59,28 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("--------------- Verifying! ---------------")
-        await verifyMessage(character.address, args)
+        await verify(character.address, args)
         log("--------------- Verify process finished! ---------------")
     } else {
         log("--------------- Localhost detected. Nothing to verify ---------------")
     }
 }
 
-module.exports.tags = ["all", "character"]
+async function handleCharacterUris() {
+    characterUris = []
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate }
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An amazing ${tokenUriMetadata.name} warrior!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log(`Uploading ${tokenUriMetadata.name}...`)
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+        tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
+    }
+    console.log("Token URIs uploaded! They are:")
+    console.log(tokenUris)
+    return tokenUris
+}
+
+module.exports.tags = ["all", "character", "main"]
