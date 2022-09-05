f995b407ecc6f1ec9f6600d5e608726521605b85
maikelordaz <maikelordaz@gmail.com>
2022-09-04 22:31:14 -0400

    Create metadataTemplate.js

---
+++ utils/metadataTemplate.js
@@ -0,0 +1,37 @@
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Strength",
+            value: 0,
+        },
+        {
+            trait_type: "Dexterity",
+            value: 0,
+        },
+        {
+            trait_type: "Constitution",
+            value: 0,
+        },
+        {
+            trait_type: "Intelligence",
+            value: 0,
+        },
+        {
+            trait_type: "Wisdom",
+            value: 0,
+        },
+        {
+            trait_type: "Charisma",
+            value: 0,
+        },
+        {
+            trait_type: "Experience",
+            value: 0,
+        },
+    ],
+}
+
+module.exports = { metadataTemplate }
